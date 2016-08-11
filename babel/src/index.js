46567e8c119b7a065a92e967820cfebf47ac9536
YuHeng Sun <butsalt@163.com>
2016-08-11 09:48:52 +0800

    add play-babel

---
+++ babel/src/index.js
@@ -0,0 +1,3 @@
+import * as butsalt from './person/butsalt';
+console.log(butsalt['default'].name === 'butsalt');
+console.log(butsalt.age === 26);
---
+++ babel/src/person/butsalt.js
@@ -0,0 +1,5 @@
+export default {
+    name: 'butsalt'
+};
+
+export let age = 26;
