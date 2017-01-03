0c7f37ce92fbec1a9b7c19337c0e965db4a2b2ff
YuHeng Sun <butsalt@163.com>
2017-01-03 23:08:52 +0800

    add play glob

---
+++ glob/test/dot.js
@@ -0,0 +1,13 @@
+var glob = require('glob');
+
+var files = glob.sync('files/a/*js');
+console.log(files);
+
+files = glob.sync('files/a/*js', {
+    dot: true
+});
+console.log(files);
+
+//'.'开头的文件名除非通过option设置否则只能也以'.'作为文件名匹配的开头才能匹配到
+files = glob.sync('files/a/.js');
+console.log(files);
