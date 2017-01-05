dd4e2b941447db205fa7525815ee3b0f8a091bc1
YuHeng Sun <butsalt@163.com>
2017-01-05 16:34:07 +0800

    improve comments on hash-key

--- glob/src/hash-key/index.js
+++ glob/src/hash-key/index.js
@@ -1,11 +1,11 @@
 var glob = require('glob');
 
-// 如果没有配置matchBase为true，*用来匹配0个到多个 文件夹/文件名 的字符
+// 如果没有配置matchBase为true，*用来匹配0个到多个 文件夹名/文件名 的字符
 var files = glob.sync('*.js');
 
 console.log(files);
 
-// 如果配置matchBase为true，在字符串内没有/的场合，传入字符串只用来匹配最终的 文件夹/文件名
+// 如果配置matchBase为true，在字符串内没有/的场合，传入字符串只用来匹配最终的 文件夹名/文件名
 files = glob.sync('*.js', {
     matchBase: true
 });
