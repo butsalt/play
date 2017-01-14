63205565ea928b8b2871af9dfa522be337d65c72
YuHeng Sun <butsalt@163.com>
2017-01-14 15:53:39 +0800

    improve comments

--- fis3/same-name-require/fis-conf.js
+++ fis3/same-name-require/fis-conf.js
@@ -1,9 +1,10 @@
+// 同文件夹下html会增加对同名js，css的依赖
+// 同文件夹下js会增将对同名css的依赖
 fis.match('*.{html,js}', {
     useSameNameRequire: true
 });
 
-// 同文件夹下html会增加对同名js，css的依赖
-// 同文件夹下js会增将对同名css的依赖
+// 默认情况下html对其他资源的依赖是不会输出到resource_map的，所以要特别声明
 fis.match('*.html', {
     useMap: true
 });
