2e25ab4554c1d2afb5c4c424ff68d3af0218eaf6
YuHeng Sun <butsalt@163.com>
2017-01-17 14:25:15 +0800

    improve comment

--- fis3/use-mod.js/fis-conf.js
+++ fis3/use-mod.js/fis-conf.js
@@ -1,5 +1,8 @@
 fis.hook('commonjs');
 
+// fis3中
+// /lib/**/*.js 只能匹配lib目录的子目录及子目录的后代目录下的所有js文件，lib目录下的文件不匹配
+// /lib/*.js 只能匹配lib目录下的所有js文件
 fis.match('/lib/**.js', {
     release: '/static/$0'
 });
