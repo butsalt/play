fcb0730658b855e08907172acbedccac1edce8b3
YuHeng Sun <butsalt@163.com>
2017-03-08 16:46:21 +0800

    add use npm example

---
+++ fis3/use-npm/fis-conf.js
@@ -0,0 +1,35 @@
+// 支持用commonjs的语法请求资源
+fis.hook('commonjs', {
+    extList: ['.js', '.jsx', '.es', '.ts', '.tsx']
+});
+
+fis.match('/node_modules/(**.js)', {
+    // npm模块需要wrapper
+    isMod: true,
+    useSameNameRequire: true,
+    release: '/static/lib/$1'
+});
+
+fis.match('/lib/**.js', {
+    release: '/static/$0'
+});
+
+// 不使用默认的fis component
+fis.unhook('components');
+
+// 通过npm查找component
+fis.hook('node_modules', {
+    // 避免在npm模块内做process的shim
+    shimProcess: false
+});
+
+// 根据模块依赖关系正确处理html文件
+fis.match('::package', {
+    postpackager: fis.plugin(
+        'loader',
+        {
+            resourceType: 'commonJs',
+            useInlineMap: true
+        }
+    )
+});
