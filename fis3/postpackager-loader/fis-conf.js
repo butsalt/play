8eb4ff4b354313eb3b8f309eb34f4f3914b60559
YuHeng Sun <butsalt@163.com>
2017-03-08 17:09:37 +0800

    improve comment on postpackager-loader

--- fis3/postpackager-loader/fis-conf.js
+++ fis3/postpackager-loader/fis-conf.js
@@ -1,6 +1,9 @@
-// 与packTo相比较，fis3-postpacker-loader可以针对每个页面的资源做打包
+// packTo可用来将几个文件在产出时打包成一个，由内置的fis3-packager-map实现
+// fis3-postpacker-loader会根据isHtmlLike文件对其他文件的依赖情况，根据配置对此文件进行加工
 fis.match('::packager', {
     postpackager: fis.plugin('loader', {
-       allInOne: true
+        // 依赖的文件将被打包成一个文件再产出
+        // pack文件将会在html对应的产出文件内被正确加载
+        allInOne: true
     })
 });
