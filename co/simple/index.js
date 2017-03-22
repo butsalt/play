63f750c2cfd0ff9670adf50e2745555999ebee64
YuHeng Sun <butsalt@163.com>
2017-03-22 13:28:21 +0800

    improve comment on co

--- co/simple/index.js
+++ co/simple/index.js
@@ -7,7 +7,11 @@ function getName () {
 }
 
 co(
-    function *() {
+    // 用generator创建一个iterator
+    function *generator () {
+        // let thenable = iterator.next();
+        // getName函数返回的thenable对象被作为此次yield的结果
+        // thenable对象被决议后iterator才被继续调用
         let name = yield getName();
         return 'Hi, ' + name;
     }
